c86f4d6eb251ce8417f630e6a611803378721395
bandura <banduraweb@ukr.net>
2020-01-10 06:06:48 +0200

    Updates

---
+++ precache-manifest.eb8ed5a184d48f838f2082da0af17f31.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e166df95c1972acde4c3c3b13c79f2f",
+    "url": "/react_redux-list-of-posts/index.html"
+  },
+  {
+    "revision": "d0abc04298b4196c7f99",
+    "url": "/react_redux-list-of-posts/static/css/2.202bdf63.chunk.css"
+  },
+  {
+    "revision": "9436f3654c0a29fe2bf3",
+    "url": "/react_redux-list-of-posts/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "d0abc04298b4196c7f99",
+    "url": "/react_redux-list-of-posts/static/js/2.b14974d6.chunk.js"
+  },
+  {
+    "revision": "9436f3654c0a29fe2bf3",
+    "url": "/react_redux-list-of-posts/static/js/main.d934064c.chunk.js"
+  },
+  {
+    "revision": "c3092fe5bcf6cd34b5b5",
+    "url": "/react_redux-list-of-posts/static/js/runtime~main.d0e8944a.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/react_redux-list-of-posts/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/react_redux-list-of-posts/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/react_redux-list-of-posts/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/react_redux-list-of-posts/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/react_redux-list-of-posts/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/react_redux-list-of-posts/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/react_redux-list-of-posts/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/react_redux-list-of-posts/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/react_redux-list-of-posts/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/react_redux-list-of-posts/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/react_redux-list-of-posts/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/react_redux-list-of-posts/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/react_redux-list-of-posts/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/react_redux-list-of-posts/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/react_redux-list-of-posts/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/react_redux-list-of-posts/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
